d19c304e70079d7e17af4159715cce0479bd9c4a
Dharmateja249 <23211a0599@gmail.com>
2026-06-27 10:40:52 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0;
+        int j=0;
+        string res;
+        while(i<word1.length()||j<word2.length()){
+            if(i<word1.length())res+=word1[i];
+            if(j<word2.length())res+=word2[j];
+            i++;
+            j++;
+        }
+        return res;
+    }
+};
