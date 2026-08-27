5025415cdbd9a6d7e379d38e60cc1671b2db9209
Dharmateja249 <23211a0599@gmail.com>
2026-08-27 17:53:34 +0530

    Add: first-missing-positive - submission-6

---
+++ Data Structures & Algorithms/first-missing-positive/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);   
+        }
+        int i=0;
+        for(int i=1;i<nums.size()+2;i++){
+            if(st.find(i)==st.end()){
+                return i;
+            }
+        }
+        return i+1;
+        
+        
+    }
+};
